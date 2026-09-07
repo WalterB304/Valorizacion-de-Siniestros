5f09e210ad3136b451962f06fb9495157f2a273c
WalterB304 <96707694+WalterB304@users.noreply.github.com>
2026-09-07 17:41:33 -0300

    Add openpyxl styles import to app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 import streamlit as st
 from datetime import datetime, date
 import io
+from openpyxl.styles import PatternFill, Font, Alignment, Border, Side
 
 st.set_page_config(page_title="Valorización de Seguro de Crédito", layout="wide")
 
